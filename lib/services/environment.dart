24dcd66782eeda60919634bce901157a0b00b286
unknown <harshitmishra5348@gmail.com>
2025-02-21 19:41:22 +0530

    branch 1

--- lib/services/environment.dart
+++ lib/services/environment.dart
@@ -1,5 +1,13 @@
 String apiUrl = "https://mtboss.com/api";
 String paymentUrl = "https://mtboss.com/api";
+
+// String apiUrl = "https://laravel.webiots.co.in/fixit/api";
+// String paymentUrl = "https://laravel.webiots.co.in/fixit/";
+
+// String apiUrl = "https://laravel.pixelstrap.net/fixit/api";
+// String paymentUrl = "https://laravel.pixelstrap.net/fixit/api";
+
+//
 Map<String, String>? headersToken(token, language) => {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
